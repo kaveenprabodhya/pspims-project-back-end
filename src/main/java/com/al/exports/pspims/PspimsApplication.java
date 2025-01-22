f3666ce2b966e45793331d00b6959ffe8b86cca3
kaveenprabodhya <kaveen.prabodhya.99@gmail.com>
2025-01-22 15:50:52 +0530

    init commit

---
+++ src/main/java/com/al/exports/pspims/PspimsApplication.java
@@ -0,0 +1,13 @@
+package com.al.exports.pspims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PspimsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PspimsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/al/exports/pspims/PspimsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.al.exports.pspims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PspimsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
